2a5808b23375d044997992672077bc66a31e870e
housejameson <sw.randels@gmail.com>
2021-11-09 23:44:38 -0600

    initial commit

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -124,5 +124,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(django_pages, 'staticfiles')
